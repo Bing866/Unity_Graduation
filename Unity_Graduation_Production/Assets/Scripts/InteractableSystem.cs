e628570f13b453d6690a137789dde98305967d4e
Bing866 <114146062+Bing866@users.noreply.github.com>
2023-08-27 22:26:57 +0800

    update 按鍵觸發對話完成

--- Unity_Graduation_Production/Assets/Scripts/InteractableSystem.cs
+++ Unity_Graduation_Production/Assets/Scripts/InteractableSystem.cs
@@ -8,6 +8,8 @@ namespace BING
     /// </summary>
     public class InteractableSystem : MonoBehaviour
     {
+        public Canvas canvas;
+
         [SerializeField, Header("第一段對話資料")]
         private DialogueData dataDialogue;
         [SerializeField, Header("對話結束後的事件")]
@@ -27,17 +29,19 @@ namespace BING
         {
             dialogueSystem = GameObject.Find("畫布對話系統").GetComponent<DialogueSystem>();
         }
-
-        // 3D 物件適用
-        // 兩個碰撞物件必須其中一個勾選 Is Trigger
-        // 碰撞開始
-        private void OnTriggerEnter(Collider other)
+        
+        private void Start()
         {
-            if (other.name.Contains(nameTarget))
+            if (canvas != null)
             {
-                print(other.name);
+                canvas.enabled = false;
+            }    
+        }
 
-                // 如果 不需要啟動道具 或者 啟動道具是顯示的 就執行 第一段對話
+        public void HitByRaycast() // 被射線打到時會進入此方法
+        {
+            if (Input.GetKeyDown(KeyCode.F)) // 當鍵盤按下 F 鍵時
+            {
                 if (propActive == null || propActive.activeInHierarchy)
                 {
                     dialogueSystem.StartDialogue(dataDialogue, onDialogueFinish);
@@ -49,14 +53,39 @@ namespace BING
             }
         }
 
-        // 碰撞結束
-        private void OnTriggerExit(Collider other)
-        {
+        // 3D 物件適用
+        // 兩個碰撞物件必須其中一個勾選 Is Trigger
 
+        // 碰撞持續 (按鍵觸發)
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.name.Contains(nameTarget))
+            {
+                canvas.enabled = true;
+                print(other.name);
+                if (Input.GetKeyDown(KeyCode.F)) 
+                { 
+                    // 如果 不需要啟動道具 或者 啟動道具是顯示的 就執行 第一段對話
+                    if (propActive == null || propActive.activeInHierarchy)
+                    {
+                        dialogueSystem.StartDialogue(dataDialogue, onDialogueFinish);
+                    }
+                    else
+                    {
+                        dialogueSystem.StartDialogue(dataDialogueActive, onDialogueFinishAfterActive);
+                    }
+                }
+            }
         }
 
-        // 碰撞持續
-        private void OnTriggerStay(Collider other)
+        // 碰撞開始 (剛體碰撞觸發)
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+
+        // 碰撞結束
+        private void OnTriggerExit(Collider other)
         {
 
         }
